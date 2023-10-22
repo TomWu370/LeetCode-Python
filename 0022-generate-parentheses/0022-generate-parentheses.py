b6366b5d5acbde11afcbf6c76a8295047a081e80
TomWu370 <75613334+TomWu370@users.noreply.github.com>
2023-10-22 16:43:52 +0100

    Time: 36 ms (85.84%), Space: 16.5 MB (70.99%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,9 +1,7 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         pairs = []
-        open = 0
-        close = 0
-        def traverse(n, pairs, open, close, temp):
+        def traverse(n, pairs, open=0, close=0, temp=""):
             if close == n:
                 pairs.append(temp)
                 return
@@ -18,26 +16,17 @@ class Solution:
                 #print(f'close', temp)
                 traverse(n, pairs, open, close+1, newTemp)
             
-
-        temp = ""
-        traverse(n, pairs, open, close, temp)
-        
+        # default for open and close are 0
+        traverse(n, pairs)
 
         return pairs
-        
-
-# ()() = pairs
-# (()) = house
-# ()()() = n=2 + 1 pair
-# ((())) = n=3 house
-# (())() = n=3 - 1 house + pair
-# ()(()) = n=3 - 1 house + pair reversed
-# (()()) = n=3-1 pair + n=3-2 house
 
 # learn binary expansion or dynamic programming
 # just check if valid form when doing DP
 # number of close can't exceed number of open
 # while less than n, keep expanding it, and while close less than open, can append 1 close
 # for each expansion, add 1 open, then if condition allow, add 1 close
-# use count tuple to record number of open and close brackets
+# record number of open and close brackets
 # Dynamic programming is all about the conditions
+# need to separate the new temporary with previous temporary, so the 2 if statement can use the previous value
+# rather than the changed value done in previous if statement
