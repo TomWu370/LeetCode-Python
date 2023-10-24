8d9d23639a02d30598386b4d3fefb6fa185dd864
TomWu370 <75613334+TomWu370@users.noreply.github.com>
2023-10-24 18:09:11 +0100

    Time: 45 ms (25.01%), Space: 16.3 MB (17.43%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -1,28 +1,34 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        a = abs(dividend)
-        b=abs(divisor)
+        # handle obvious overflor
+        if dividend > 2147483648-1:
+            return 2147483647
+        elif dividend < -2147483648:
+            return -2147483648
+
+        # handle ans calculation overflow
+        if (dividend == -2147483648 and divisor == -1) or (dividend == 2147483648-1 and divisor == 1):
+            return 2147483647
+
+        ans = 0
+        newDividend, newDivisor = abs(dividend), abs(divisor)
         
-        negative = (dividend<0 and divisor>=0) or (dividend>=0 and divisor<0)
-        
-        output = 0
-        
-        while a>=b:
+        while newDividend >= newDivisor:
+            multiplier = newDivisor
             counter = 1
-            decrement = b
-            
-            while a>=decrement:
-                a-=decrement
-                
-                output+=counter
-                counter+=counter
-                decrement+=decrement
-                
-        output = output if not negative else -output
+
+            while newDividend >= multiplier:
+                newDividend -= multiplier
+                multiplier += multiplier
+                ans += counter
+                counter += counter
         
-        return min(max(-2147483648, output), 2147483647)
+        if (dividend < 0) ^ (divisor < 0):
+            ans = -ans
+        return ans 
         
 
 # perform long division with subtraction
 # to avoid dealing with both parameter being negative, get the absolute value of both at the start
 # then only is negative then convert ans to negative
+# in order to decrease the run time, increase the divisor each time, then for the last step do it step by step
