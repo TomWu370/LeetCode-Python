f73ee759bc3507c744ebaa499852eca988d4230d
TomWu370 <75613334+TomWu370@users.noreply.github.com>
2023-11-02 00:22:16 +0000

    Time: 37 ms (61.59%), Space: 16.4 MB (26.2%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,9 +1,24 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        words = list(filter(None, s.split(' ')))
-        return len(words[-1])
+        answer = ''
+        for i in reversed(s):
+            print(i)
+            if i == ' ' and not answer:
+                continue
+            elif i == ' ':
+                break
+            else:
+                answer += i
+
+        return len(answer)
 
 
 # essentially delimit by space then return last item in list
 # and finally, remove empty strings for edge cases where there are space at the end
 # use filter to replace easily, wrap result with list to get the final list
+
+# more efficient way includes pointers or iterator
+# traverse each item in the string backwards
+# reversed can reverse most iterators
+# if answer is empty then ignore space until answer is not empty
+# keep appending non space to answer
