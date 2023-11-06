c8b7e07981729633427342fb8baaaa1f6a56632b
TomWu370 <75613334+TomWu370@users.noreply.github.com>
2023-11-06 01:04:53 +0000

    Time: 65 ms (52.75%), Space: 17.8 MB (31.05%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -17,4 +17,11 @@ class Solution:
 # if bumpy then take multple stock, check if next price is less than current, if so then sell current stock then buy next stock
 # if j is less than i then move i along and make j to i + 1, and restart iteration
 
-# intuition
+# intuition, loop through prices like in 121, except for the if loop, check the next 2 values, so make sure the current
+# j is within range
+# if the next j value is more than the current j, then tally up the current stock price, then move i to the next j/buy next j stock
+# if the 3rd condition is to make sure descending list aren't taken into account
+# in the else statement, check whether the i is less than j value, this takes care of ascending list, as there are no stopping
+# value for ascending list, asc and desc list are edge cases for this question
+# if i is less than j in ascending list then tally up the whole list until it's not ascending
+# finally move the i value along 1, to restart the list
