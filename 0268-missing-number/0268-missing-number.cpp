821ae747f49159782caba3af887c4f3a0bb501b6
TomWu370 <75613334+TomWu370@users.noreply.github.com>
2023-12-05 23:40:04 +0000

    Time: 21 ms (15.01%), Space: 18.4 MB (31.74%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
 
-        std::vector<int> integerList(nums.size()+1);
-
-        // Use std::iota to generate the integer list
-        std::iota(integerList.begin(), integerList.end(), 0);
-        // difference only works with sorted lists
+        int expected = 0;
         std::sort(nums.begin(), nums.end());
-        std::sort(integerList.begin(), integerList.end());
-        std::vector<int> diff;
-
-        std::set_symmetric_difference(nums.begin(), nums.end(), integerList.begin(), integerList.end(), 
-        std::inserter(diff, diff.begin()));
-        return diff[0];
+        int length = nums.size();
+        for (int i = 0; i < length; i++){
+            if (nums[i] != expected){
+                return expected;
+            }
+            expected++;
+        }
+        return expected;
     } 
 };
 
