be4873e247e8516da9d2d9a48b8adfed7675ae71
TomWu370 <75613334+TomWu370@users.noreply.github.com>
2023-12-08 18:55:37 +0000

    Time: 7 ms (28.95%), Space: 9.4 MB (75.33%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        string ans = "";
+        char currChar = chars[0];
+        int count = 0;
+        for (auto c : chars){
+            if (c == currChar){
+                count ++;
+            }
+            else{
+                ans += currChar;
+                if (count > 1){
+                    ans += to_string(count);
+                }
+                currChar = c;
+                count = 1;
+            }
+        }
+        ans += currChar;
+        if (count >1){
+            ans += to_string(count);
+        }
+
+        for (int i = 0; i < ans.size(); i++){
+            chars[i] = ans[i];
+        }
+        chars.resize(ans.size());
+        return chars.size();
+        
+    }
+};
+
+// see python for implementation details
+// to do inplace vector replacing in c++ you simply reassign values
+// to resize inplace, simply use .resize()
