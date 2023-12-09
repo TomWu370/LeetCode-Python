ab5086a7309709cb40a4bd2033bfaa5fb876b5ad
TomWu370 <75613334+TomWu370@users.noreply.github.com>
2023-12-09 23:17:40 +0000

    Time: 3 ms (88.10%), Space: 7.7 MB (56.93%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.cpp
+++ 0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     string reverseStr(string s, int k) {
         int length = s.size();
-        
         for (int i = 0; i<length; i += 2*k){
             if (i+k <= length){
             reverse(s.begin()+i, s.begin()+i+k);
