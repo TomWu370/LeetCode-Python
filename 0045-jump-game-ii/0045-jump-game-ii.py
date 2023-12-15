e905af18c0f3dc5967522bc12226ee0eaada96d5
TomWu370 <75613334+TomWu370@users.noreply.github.com>
2023-12-15 01:31:09 +0000

    Time: 119 ms (63.86%), Space: 17.5 MB (31.80%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,14 +1,19 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        jumps = 0
+        current, furthest = 0, nums[0]
+        for i in range(len(nums)-1):
+            # after calculating a jump, continue the next spots, to see if there's a further jump
+            # i.e if can jump 2 squares, then you scan the next 2 squares and get the furthest value
+            furthest = max(furthest, i+nums[i])
+            if i == current:
+                # performs the jump
+                jumps += 1
+                current = furthest
+            if current >= len(nums)-1:
+                return jumps
 
-        dp = [math.inf] * len(nums)
-        dp[0] = 0
-
-        for i in range(len(nums)):
-            for j in range(nums[i]+1):
-                if i+j < len(nums):
-                    dp[i+j] = min(dp[i] + 1, dp[i+j])
-        return dp[len(nums)-1]
+        return jumps
             
 # intuition, declare a empty list, opposite of what we're trying to achieve
 # or the opposite or minimise, so infinite
@@ -17,3 +22,6 @@ class Solution:
 # for each step available, check if the step will cause index to go past target
 # if not then update the current best steps, to be the minimum of previously possible defined minimum values
 # all the way to the last item, which we return
+# intuition for time reduction, the assumption, we can always reach the end
+# this means we can have a greedy approach, let's say we can reach 3,1,end
+# maybe going to 1 is optimal, however anything 1 can reach can be reached by 3, this is why greedy works here
