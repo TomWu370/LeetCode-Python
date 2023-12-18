789d683d8755de999868aa9323556ec0ed3b9a38
TomWu370 <75613334+TomWu370@users.noreply.github.com>
2023-12-18 15:23:48 +0000

    Time: 42 ms (22.91%), Space: 16.2 MB (53.53%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        memo = {}
+        memo[0] = 0
+        memo[1] = 1
+        memo[2] = 1
+        for i in range(3,n+1):
+            memo[i] = memo[i-1] + memo[i-2] + memo[i-3]
+        return memo[n]
+
+# intuition, tribonacci is just fibonacci but with 3 numbers and 3 base cases
+# rather than using iteration, it's easier to use bottom up approach, since there are no new values during calculations
+# since we have already set the base cases, and for each new iteration it is only using previously defined dictionary value
