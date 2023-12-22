3c7b14de59b7a778f91a324e31aec00642ac05a4
TomWu370 <75613334+TomWu370@users.noreply.github.com>
2023-12-22 22:49:22 +0000

    Time: 42 ms (62.17%), Space: 17.4 MB (6.52%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
@@ -1,6 +1,5 @@
 class Solution:
     def maxScore(self, s: str) -> int:
-
         scores = []
         for i in range(1, len(s)):
             left = s[:i]
@@ -14,4 +13,4 @@ class Solution:
 # intuition, iterate through splitting string, start from 1
 # keep record of number of 0 on left and number of 1 on right, sum of those 2 number is the total for this split
 # keep record of all the splits, then return the max of the list
-# edge case, when length is less than 3, return the max of 1 counts or 0 counts
+# since slices are inclusive, no need to worry about out of index with len(s)
