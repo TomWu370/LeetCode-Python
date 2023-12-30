f5b5d0de246ca7a15bf80db001fe584a491fe511
TomWu370 <75613334+TomWu370@users.noreply.github.com>
2023-12-30 01:22:15 +0000

    Time: 100 ms (5.08%), Space: 17.3 MB (9.52%) - LeetHub

---
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.py
@@ -0,0 +1,11 @@
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        length = len(words)
+        counts = Counter("".join(words))
+        for count in counts:
+            if counts[count] % length != 0:
+                return False
+        return True
+
+# intuition, words are equal if there are enough unique characters that can be evenly distributed to all words in words list
+# in order words, use Counter, and check if each item can be evenly divided by the length of words
