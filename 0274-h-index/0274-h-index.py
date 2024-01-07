4b211a84803dd44af4268a3973c096fe31a0da2a
TomWu370 <75613334+TomWu370@users.noreply.github.com>
2024-01-07 00:09:47 +0000

    Time: 37 ms (91.44%), Space: 17.7 MB (16.49%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,13 +1,10 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         citations.sort()
-        print(citations)
         for i in range(len(citations), 0, -1):
-            compare = len(citations) -2 - i
-            print(f' i {i} : {compare}')
             if citations[len(citations) - i] >= i:
                 return i
-        return 1 if citations[0] >= 1 else 0
+        return 0
 
 # intuition, go from big to small, iterate backward from len of citations
 # check if there are at most h paper, with atleast h citations
